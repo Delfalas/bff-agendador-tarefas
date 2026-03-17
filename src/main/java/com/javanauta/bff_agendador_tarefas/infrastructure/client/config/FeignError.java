f22376a82bbc64acdf5cf844c95d7a703956b0e0
Delfalas <vinidantas07@hotmail.com>
2026-03-17 20:26:25 -0300

    added via cep api

--- src/main/java/com/javanauta/bff_agendador_tarefas/infrastructure/client/config/FeignError.java
+++ src/main/java/com/javanauta/bff_agendador_tarefas/infrastructure/client/config/FeignError.java
@@ -2,32 +2,48 @@ package com.javanauta.bff_agendador_tarefas.infrastructure.client.config;
 
 import com.javanauta.bff_agendador_tarefas.infrastructure.exceptions.BusinessException;
 import com.javanauta.bff_agendador_tarefas.infrastructure.exceptions.ConflictException;
+import com.javanauta.bff_agendador_tarefas.infrastructure.exceptions.IllegalArgumentException;
 import com.javanauta.bff_agendador_tarefas.infrastructure.exceptions.ResourceNotFoundException;
 import com.javanauta.bff_agendador_tarefas.infrastructure.exceptions.UnauthorizedException;
 import feign.Response;
 import feign.codec.ErrorDecoder;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+
 public class FeignError implements ErrorDecoder {
 
     @Override
     public Exception decode(String s, Response response) {
+
+        String mensagemErro = mensagemErro(response);
+
         System.out.println("STATUS: " + response.status()); System.out.println("URL: " + response.request().url());
+
         switch (response.status()) {
             case 409:
-                return new ConflictException("Registro já existente!");
+                return new ConflictException("Erro: " + mensagemErro);
             case 400:
-                return new BusinessException("Erro de validação!");
-            case 403:
-                return new UnauthorizedException("Acesso negado - Token inválido ou sem permissão!");
+                return new IllegalArgumentException("Erro: " + mensagemErro);
+            case 403, 404:
+                return new ResourceNotFoundException("Erro: " + mensagemErro);
             case 401:
-                return new UnauthorizedException("Autenticacao ou Token invalida!");
-            case 500:
-                return new BusinessException("Erro interno no microsserviço!");
-            case 404:
-                return new ResourceNotFoundException("Recurso não encontrado!");
+                return new UnauthorizedException("Erro: " + mensagemErro);
             default:
-                return new BusinessException("Erro de servidor! ");
+                return new BusinessException("Erro: " + mensagemErro);
 
+        }
+    }
+
+    private String mensagemErro(Response response) {
+        try {
+            if(Objects.isNull(response.body())) {
+                return "";
+            }
+            return new String(response.body().asInputStream().readAllBytes(), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
